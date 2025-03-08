e68181143d03412f5280637e63d96479818eea9e
darkoto12 <darkoto12@gmail.com>
2025-03-09 00:02:37 +0200

    build

--- gulp/tasks/js.js
+++
@@ -1,9 +0,0 @@
-import fileInclude from 'gulp-file-include';
-
-export const js = () => {
-  return app.gulp
-  .src(`${app.path.srcFolder}/js/*.js`)
-  .pipe(fileInclude())
-  .pipe(app.gulp.dest(`${app.path.buildFolder}/js`))
-  .pipe(app.plugins.browsersync.stream());
-}
